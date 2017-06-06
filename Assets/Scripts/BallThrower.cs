afb271725895ebd0d05ae38d25bedb77101dcf3d
Mark <umbreon27@gmail.com>
2017-06-05 21:21:08 -0700

    Ball throwing physics.
    Added ball throwing mechanic with curveball physics.

---
+++ Assets/Scripts/BallThrower.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallThrower : MonoBehaviour {
+
+    public GameObject ballPrefab;
+
+    public float tossForce = 5;
+
+    public Vector3 spin;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")) { tossBall(transform.forward, tossForce); }
+	}
+
+    public GameObject tossBall(Vector3 direction, float force)
+    {
+        GameObject ball = Instantiate<GameObject>(ballPrefab);
+        ball.transform.position = transform.position;
+        Rigidbody rb = ball.GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = 999999;
+        rb.AddForce(direction.normalized * force, ForceMode.VelocityChange);
+        rb.AddTorque(spin, ForceMode.VelocityChange);
+        return ball;
+    }
+}
---
+++ Assets/Scripts/CurveBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurveBall : MonoBehaviour {
+
+    public float curvePower = 1;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float velMag = rb.velocity.magnitude;
+        Vector3 velDirection = rb.velocity.normalized;
+        Vector3 curveDirection = (-rb.GetPointVelocity(transform.position + velDirection) - rb.velocity);
+        rb.AddForce((curveDirection - velDirection) * velMag * curvePower, ForceMode.Acceleration);
+    }
+}
